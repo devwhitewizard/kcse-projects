04403911b37c0e4aec416e4b4791e5e6dd302582
White Wizard <jonathanmwanza162@gmail.com>
2026-03-08 06:15:14 +0300

    changed from Browserouter to hae=rshrooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
@@ -7,28 +7,11 @@ import Contact from "./pages/Contact.jsx";
 import PastProjects from "./pages/PastProjects.jsx";
 import Shop from "./pages/Shop.jsx";
 import Terms from "./pages/Terms.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   const location = useLocation();
 
-  const renderPage = () => {
-    switch(location.pathname) {
-      case '/':
-        return <Home />;
-      case '/about':
-        return <About />;
-      case '/contact':
-        return <Contact />;
-      case '/projects':
-        return <PastProjects />;
-      case '/shop':
-        return <Shop />;
-      case '/terms':
-        return <Terms />;
-      default:
-        return <Home />;
-    }
-  };
 
   useEffect(() => {
     // Ensure each route navigation starts at top of page
@@ -41,7 +24,15 @@ function App() {
 
   return (
     <>
-      {renderPage()}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<PastProjects />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/NotFound.css';
+
+export default function NotFound() {
+  return (
+    <div className="not-found-container">
+      <h1>404</h1>
+      <p>The page you are looking for cannot be found.</p>
+    </div>
+  );
+}
